f9af664be88c9cd690a3899ba2297c37f55da6c4
mclrc <moritzamando@proton.me>
2024-03-02 15:20:42 +0100

    Run tests once

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import reactPlugin from 'vite-plugin-react';
+import process from 'process';
 
 export default {
   // @ts-ignore
   jsx: 'react',
-  plugins: process.env.NODE_ENV === "test" ? [] : [reactPlugin],
+  plugins: process.env.NODE_ENV === 'test' ? [] : [reactPlugin],
   test: {
     include: ['**/*.test.ts'],
   },
